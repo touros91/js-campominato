4997096519b4af93c19125bcae07455ea4595214
touros91 <andreageraci91@gmail.com>
2021-09-02 14:06:23 +0200

    Aggiunto interazione utente e validazione  gioco

--- js/main.js
+++ js/main.js
@@ -15,20 +15,38 @@
 
 // versione con tra 1 e 20 e 3 bombe 
 
+
+// FUNZIONI 
+
+// creo la funzione che disegna in pagina la griglia con massimo 10 celle per riga
+
+function griglia10 (celle) {
+    for (var i = 1; i <= celle; i++) {
+        let campo = document.getElementById("campo");
+        campo.innerHTML += `<div class="cella">${i}</div>`;
+    }
+}
+
+
 // 1. creo la funzione che genera un numero random 
 
 function numeroRandom (min, max) {
     return Math.floor( Math.random() * (max - min + 1) ) + min;
 }
 
+// PROGRAMMA PRINCIPALE
+
 // 2. creo l'array bombe 
+
+
+
 
 var bombe = [];
 
 // 3. fino a quando la lunghezza dell'array è minore di 3, genero un numero casuale, se non è presente nell'array lo pusho dentro l'array 
 
 while (bombe.length <= 2) {
-    var numeroCasuale = numeroRandom(1, 20);
+    var numeroCasuale = numeroRandom(1, 10);
     if (bombe.includes(numeroCasuale) == false) {
         bombe.push(numeroCasuale);
     }
@@ -36,41 +54,94 @@ while (bombe.length <= 2) {
 
 console.log(bombe);
 
+// 4. creo un evento click su una cella che mostra il contenuto della stessa e cambia il colore di background in rosso 
+var celleUtente = [];
 
-// FUNZIONE 
 
-// creo la funzione che disegna in pagina la griglia con massimo 10 celle per riga
+var numeroCelle = 10;
 
-function griglia10 (celle) {
-    for (var i = 1; i <= celle; i++) {
-        document.getElementById("campo").innerHTML += `<div class="cella">${i}</div>`;
-    }
-}
+console.log(bombe.length);
 
-// PROGRAMMA PRINCIPALE 
+var celleLibere = numeroCelle - bombe.length;
+console.log(celleLibere);
 
-// 1. chiedo all'utente di inserire un numero di celle compreso tra 1 e 100
-
-// let numeroCelle = parseInt(prompt(`Inserisci un numero di celle compreso tra 1 e 100`));
-
-// 2. validazione: verifico se il numero inserito dall'utente è compreso tra 1 e 100, in caso negativo gli chiedo di reinserirlo fino a quando non è compreso
-
-while (isNaN(numeroCelle) || numeroCelle < 1 || numeroCelle > 100) {
-    alert("Errore!");
-    numeroCelle = parseInt(prompt(`Inserisci un numero di celle compreso tra 1 e 100`)); 
-}
-
-// 3. chiamo(invoco) la funzione griglia10
+var scelta = 0;
 
 griglia10(numeroCelle);
 
-// 4. creo un evento click su una cella che mostra il contenuto della stessa e cambia il colore di background in rosso 
-   
-document.getElementById("campo").addEventListener(`click`,
+let campo = document.getElementById("campo");
+let CountButtonHomeClicks = -1;
+
+campo.addEventListener(`click`,
     function(evento) {
-        alert(evento.target.innerHTML);
-        evento.target.classList.toggle("bg-red");
-    }
+        scelta += 1; 
+        CountButtonHomeClicks += 1;
+        let sceltaUtente = parseInt(evento.target.innerHTML);
+        celleUtente.push(sceltaUtente);
+        while (bombe.includes(sceltaUtente) == true) {
+            evento.target.classList.add("bg-red");
+            alert("BOMBA! BOOM! SEI ESPLOSO!");
+            return alert("Il tuo punteggio è: " + CountButtonHomeClicks);
+        } 
+
+        if (bombe.includes(sceltaUtente) == false && scelta < celleLibere) {
+            evento.target.classList.add("bg-white");
+            alert("Bravo non hai beccato nessuna bomba! Continua cosi!");
+            } 
+            
+        if (scelta === celleLibere) {
+            return alert("HAI VINTO! NON HAI BECCATO NESSUNA BOMBA!");
+        } 
+
+
+console.log(scelta);
+
+        }
+        
+    
 );
+
+
+
+console.log(celleUtente);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // FUNZIONE 
+
+// // creo la funzione che disegna in pagina la griglia con massimo 10 celle per riga
+
+// function griglia10 (celle) {
+//     for (var i = 1; i <= celle; i++) {
+//         document.getElementById("campo").innerHTML += `<div class="cella">${i}</div>`;
+//     }
+// }
+
+// // PROGRAMMA PRINCIPALE 
+
+
+// griglia10(10);
+
+// // 4. creo un evento click su una cella che mostra il contenuto della stessa e cambia il colore di background in rosso 
+   
+// document.getElementById("campo").addEventListener(`click`,
+//     function(evento) {
+//         alert(evento.target.innerHTML);
+//         evento.target.classList.toggle("bg-white");
+//     }
+// );
 
 
